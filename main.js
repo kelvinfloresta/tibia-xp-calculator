a882b9100a12e1f8f77a9e40208abdd2fab1de96
BastosGabs <44577986+BastosGabs@users.noreply.github.com>
2023-05-16 12:50:05 -0300

    refactor: remove useless math funcs

--- main.js
+++ main.js
@@ -2,82 +2,88 @@ const {
   scenarios,
   rawXpPartyPerHour,
   rawXpSoloPerHour,
-  xpMultiplier,
-} = require("./scenarios");
+  xpMultiplier
+} = require('./scenarios')
 
-const { printTable } = require("console-table-printer");
-const { format, multiply, subtract, add } = require("mathjs");
+const { printTable } = require('console-table-printer')
+const { format } = require('mathjs')
 
-function calculateFullXP({ rawXpPerhour, huntingHours, bonusHours }) {
+function calculateFullXP ({ rawXpPerhour, huntingHours, bonusHours }) {
   if (huntingHours === 0) {
-    return { normal: 0, bonus: 0, total: 0 };
+    return { normal: 0, bonus: 0, total: 0 }
   }
 
-  const normalHours = subtract(huntingHours, bonusHours);
+  const normalHours = huntingHours - bonusHours
   const normal = calculateXP({
     huntingHours: normalHours,
     rawXpPerhour,
-    multiplier: xpMultiplier,
-  });
+    multiplier: xpMultiplier
+  })
 
   const bonus = calculateXP({
     huntingHours: bonusHours,
     rawXpPerhour,
-    multiplier: multiply(xpMultiplier, 1.5),
-  });
+    multiplier: xpMultiplier * 1.5
+  })
 
-  const total = +format(add(normal, bonus), { precision: 10 });
+  const total = precision(normal + bonus)
 
-  return { normal, bonus, total };
+  return { normal, bonus, total }
 }
 
-function calculateXP({ huntingHours, rawXpPerhour, multiplier }) {
-  return +format(multiply(huntingHours, rawXpPerhour, multiplier), {
-    precision: 10,
-  });
+function calculateXP ({ huntingHours, rawXpPerhour, multiplier }) {
+  return +format(huntingHours * rawXpPerhour * multiplier, {
+    precision: 10
+  })
 }
 
-function formatKK({ solo, party, total, description }) {
+function precision (value) {
+  return +format(value, {
+    precision: 10
+  })
+}
+
+function formatKK ({ solo, party, total, description }) {
   return {
     description,
-    Solo: solo + " kk",
-    Party: party + " kk",
-    Total: total + " kk",
-  };
+    Solo: solo + ' kk',
+    Party: party + ' kk',
+    Total: total + ' kk'
+  }
 }
 
-function print(scenarios) {
-  const data = scenarios.sort((a, b) => b.total - a.total).map(formatKK);
+function print (scenarios) {
+  const data = scenarios.sort((a, b) => b.total - a.total).map(formatKK)
 
-  console.log(`\n\n`);
-  console.log(`XP/h PT: ${rawXpPartyPerHour}kk`);
-  console.log(`XP/h Solo: ${rawXpSoloPerHour}kk`);
-  console.log(`\n`);
-  printTable(data);
-  console.log(`\n\n`);
+  console.log('\n\n')
+  console.log(`XP/h PT: ${rawXpPartyPerHour}kk`)
+  console.log(`XP/h Solo: ${rawXpSoloPerHour}kk`)
+  console.log('\n')
+  printTable(data)
+  console.log('\n\n')
 }
 
-function split({ hoursSolo, hoursParty, bonusHoursParty, bonusHoursSolo }) {
+function split ({ hoursSolo, hoursParty, bonusHoursParty, bonusHoursSolo }) {
   const { total: soloTotal } = calculateFullXP({
     rawXpPerhour: rawXpSoloPerHour,
     bonusHours: bonusHoursSolo,
-    huntingHours: hoursSolo,
-  });
+    huntingHours: hoursSolo
+  })
 
   const { total: partyTotal } = calculateFullXP({
     rawXpPerhour: rawXpPartyPerHour,
     bonusHours: bonusHoursParty,
-    huntingHours: hoursParty,
-  });
+    huntingHours: hoursParty
+  })
 
-  const description = `${hoursSolo} horas solo + ${hoursParty} horas PT`;
+  const description = `${hoursSolo} horas solo + ${hoursParty} horas PT`
 
   return {
     party: partyTotal,
     solo: soloTotal,
-    total: add(soloTotal, partyTotal),
-    description,
-  };
+    total: soloTotal + partyTotal,
+    description
+  }
 }
 
-print(scenarios.map(split));
+print(scenarios.map(split))
