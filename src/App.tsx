4b2dbb8ec74f525e703341f02e712b2c3fb8e518
Kelvin Floresta <kelvin.floresta@frontdoorhome.com>
2023-05-18 00:10:26 -0300

    feat: migrate to react

---
+++ src/App.tsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react';
+
+import Card from 'antd/lib/card';
+import Input from 'antd/lib/input';
+import Form from 'antd/lib/form';
+
+import styled from 'styled-components';
+import { Space, Table, Typography } from 'antd';
+import { HuntCalculator } from './logic/HuntCalculator';
+import Column from 'antd/es/table/Column';
+
+const Background = styled.div`
+	padding-top: 10vh;
+	margin: 0 auto;
+	height: 100vh;
+	box-sizing: border-box;
+	width: 100%;
+	background-color: #dcdcdc;
+`;
+
+function render(e: any) {
+	if (!e || isNaN(e)) {
+		return '-';
+	}
+
+	return e + ' kk';
+}
+
+function App() {
+	const [form, setForm] = useState<any>({});
+	const [calculator, setCalculator] = useState<HuntCalculator | null>(null);
+
+	useEffect(() => {
+		const calculator = new HuntCalculator(form.rawXpPartyPerHour, form.rawXpSoloPerHour);
+		setCalculator(calculator);
+	}, [form.rawXpPartyPerHour, form.rawXpSoloPerHour]);
+
+	function onChange({ name, value }: { name: string; value: string }) {
+		const newForm = { ...form, [name]: value };
+		setForm(newForm);
+	}
+
+	const result =
+		calculator?.calculate({
+			hoursSolo: form.huntHours,
+			hoursParty: form.huntHours,
+			bonusHoursParty: form.bonusHours || 0,
+			bonusHoursSolo: form.bonusHours || 0,
+		}) || {};
+
+	return (
+		<Background>
+			<Card style={{ maxWidth: 600, margin: '0 auto' }}>
+				<Form
+					onChange={(e: any) => onChange(e.nativeEvent.target)}
+					style={{ maxWidth: 600 }}
+					labelAlign="left"
+					autoComplete="off"
+				>
+					<Space size={64} direction="horizontal">
+						<div>
+							<Typography.Title style={{ textAlign: 'center' }} level={3}>
+								Solo
+							</Typography.Title>
+							<Form.Item label="Raw XP/h">
+								<Input name="rawXpSoloPerHour" type="outline" />
+							</Form.Item>
+
+							<Form.Item label="Horas de hunt?">
+								<Input name="huntHours" />
+							</Form.Item>
+						</div>
+						<div>
+							<Typography.Title style={{ textAlign: 'center' }} level={3}>
+								Party
+							</Typography.Title>
+							<Form.Item label="Raw XP/h">
+								<Input name="rawXpPartyPerHour" />
+							</Form.Item>
+
+							<Form.Item label="Horas stamina verde?">
+								<Input name="bonusHours" />
+							</Form.Item>
+						</div>
+					</Space>
+				</Form>
+
+				<Table dataSource={[result]} pagination={false}>
+					<Column dataIndex="solo" title="XP Solo" render={render}></Column>
+					<Column dataIndex="diff" title="Horas a mais"></Column>
+					<Column dataIndex="party" title="XP Party" render={render}></Column>
+				</Table>
+			</Card>
+		</Background>
+	);
+}
+
+export default App;
